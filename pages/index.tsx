dd4bca10bb09247c8628a68218acc9f59378cf83
Haidar Ezio <haidar.ezio2014@gmail.com>
2023-02-04 13:28:00 +0100

    a bit of housekeeping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-8xl">Hello Next + Tailwind !</h1>
+      <h1 className="font-bold text-center text-8xl">Hello Next + Tailwind !</h1>
     </>
   );
 }
