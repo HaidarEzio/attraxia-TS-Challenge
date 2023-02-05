7c81f417b8d69629812637eacd68043c8778d263
Haidar Ezio <haidar.ezio2014@gmail.com>
2023-02-06 00:26:00 +0100

    this will make vercel happy

--- pages/components/reusable/badge.reusable.tsx
+++ pages/components/reusable/badge.reusable.tsx
@@ -1,4 +1,4 @@
-const Badge = ({ children }) => {
+const Badge = ({ children }: any) => {
   return (
     <span className="flex items-center justify-center w-6 h-6 font-medium bg-green-600 rounded-full items">
       <span>{children}</span>
