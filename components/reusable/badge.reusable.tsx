d1aec580bb2f2f81c4c752fee6c2ec921eddaa3e
Haidar Ezio <haidar.ezio2014@gmail.com>
2023-02-05 23:51:12 +0100

    changed their placements

--- components/reusable/badge.reusable.tsx
+++
@@ -1,9 +0,0 @@
-const Badge = ({ children }) => {
-  return (
-    <span className="flex items-center justify-center w-6 h-6 bg-green-600 rounded-full items ">
-      <span>{children}</span>
-    </span>
-  );
-};
-
-export default Badge;
