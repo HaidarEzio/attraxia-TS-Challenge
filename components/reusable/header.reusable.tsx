fda614b8940eaa64f6ae0ea23f743b5a6a86e2ec
Haidar Ezio <haidar.ezio2014@gmail.com>
2023-02-05 22:49:24 +0100

    fixed top header styling

--- components/reusable/header.reusable.tsx
+++ components/reusable/header.reusable.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+
 const Header = () => {
   return (
-    <div className="flex items-center justify-between px-8 py-6 mt-10 bg-white ">
-      <h3 className="font-bold">Support Resources</h3>
+    <div className="flex items-center justify-between px-8 py-6 mt-10 bg-white">
+      <h3 className="font-bold">Support &nbsp;Resources</h3>
       <div className="flex items-center space-x-5">
         <h4 className="">Need Help?</h4>
         <button className="flex items-center py-3 space-x-2 bg-blue-600 rounded-lg px-7 ">
