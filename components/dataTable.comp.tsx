4c02c72603bf9b58b2645b5a7c2e8a8a345e7716
Haidar Ezio <haidar.ezio2014@gmail.com>
2023-02-06 03:08:35 +0100

    fixing some more issues

--- components/dataTable.comp.tsx
+++ components/dataTable.comp.tsx
@@ -1,48 +1,7 @@
-import { TableDataType, defaultData } from "../../utils/placeholder.data";
-import { useTable, Column, useFilters } from "react-table";
-import { useState, useMemo, useEffect } from "react";
-import Image from "next/image";
-import DropDown from "./reusable/dropdown.reusable";
+import { useTable, useFilters } from "react-table";
+import { useEffect } from "react";
 import { useAtom } from "jotai";
 import { statusAtom, ticketAtom } from "@/pages";
-
-// const columns = [
-//   columnHelper.accessor("ticket", {
-//     header: () => <p className="text-sm text-start">Ticket</p>,
-//     cell: (title) => (
-//       <div className="flex flex-col ">
-//         <h3 className="text-lg font-bold text-blue-700">{title.getValue()}</h3>
-//         <p className="text-base text-neutral-600 ">{title.row.original.description}</p>
-//       </div>
-//     ),
-//   }),
-//   columnHelper.accessor("status", {
-//     header: () => <p className="px-10 text-sm text-start">Status</p>,
-//     cell: (data) => (
-//       <div className="flex justify-center">
-//         <span className="px-3 py-1 text-center bg-red-500 rounded-full">{data.getValue()}</span>
-//       </div>
-//     ),
-//   }),
-//   columnHelper.accessor("createdOn", {
-//     header: () => <p className="text-start">Created On</p>,
-//   }),
-//   columnHelper.accessor("replies", {
-//     header: () => <p className="text-start">Replies</p>,
-//     cell: (data) => (
-//       <div className="flex space-x-2">
-//         <Image className="rounded-full" src={data.row.original.replies.imgUrl} alt="img" height={20} width={20} />
-//         <p>Last by {data.row.original.replies.name}</p>
-//         <p className="text-white bg-black rounded-full ">{data.row.original.replies.replyNumber}</p>
-
-//         {data.row.original.replies.staff ? <p className="">staff</p> : null}
-//       </div>
-//     ),
-//   }),
-// ];
-// interface DataTableProps {
-//   columns: Column<TableDataType>[];
-// }
 
 const DataTable = ({ columns, data }: any) => {
   const [status] = useAtom(statusAtom);
@@ -55,9 +14,9 @@ const DataTable = ({ columns, data }: any) => {
     useFilters
   );
 
-  // Listen for input changes outside
+  //? Listen for input changes outside
   useEffect(() => {
-    // This will now use our custom filter for age
+    // This will now use our custom filter for the status column
     setFilter("status", status);
     setFilter("ticket", ticket);
   }, [status, setFilter, ticket]);
@@ -65,7 +24,7 @@ const DataTable = ({ columns, data }: any) => {
   return (
     <>
       <table {...getTableProps()} className="w-full">
-        <thead className="px-5">
+        <thead>
           {headerGroups.map((headerGroup, idx) => (
             <tr {...headerGroup.getHeaderGroupProps()} key={idx}>
               {headerGroup.headers.map((column, idx) => (
