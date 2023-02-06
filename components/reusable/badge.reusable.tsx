f2471a68f2e74829d1582d104837ec60fb3bd3a8
Haidar Ezio <haidar.ezio2014@gmail.com>
2023-02-06 01:45:20 +0100

    badge color variants

--- components/reusable/badge.reusable.tsx
+++ components/reusable/badge.reusable.tsx
@@ -1,13 +1,14 @@
 import tw, { styled } from "twin.macro"; //eslint-disable-line
 
 const BadgeStitched = styled.span({
-  ...tw`flex items-center justify-center w-6 h-6 font-medium rounded-full text-[#333333]`,
+  ...tw`flex items-center justify-center w-6 h-6 font-bold rounded-full text-[#333333]`,
   variants: {
     color: {
       black: tw`text-white bg-black`,
-      green: tw`bg-green-600`,
-      red: tw`bg-red-600`,
-      gray: tw`bg-neutral-200`,
+      green: tw`text-white bg-[#52D194]`,
+      lightBlue: tw`bg-[#286EF1] text-white`,
+      darkBlue: tw`bg-[#9026B1] text-white`,
+      gray: tw`bg-[#E6E6E6] text-black`,
     },
   },
 });
